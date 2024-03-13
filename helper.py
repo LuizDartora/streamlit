24206599c40ef5c6ac825ff841b2be587efc949d
Luiz Carlos <you@example.com>
2024-03-13 16:34:47 -0300

    a

--- helper.py
+++ helper.py
@@ -1,4 +1,8 @@
 import datetime
+import pandas as pd
+import altair as alt
+import streamlit as st
+import decimal
 
 def get_ano():
     today = datetime.datetime.today()
@@ -112,3 +116,101 @@ def check_se_diesel(lista_modelos):
             nova_lista_modelos.append(modelo)
     return nova_lista_modelos
 
+def make_graph(df, min_value, max_value):
+    chart = alt.Chart(df).mark_line(point=alt.OverlayMarkDef(filled=False, fill="white", size=100), size=5).encode(
+        alt.X('Data',sort=None, axis=alt.Axis(grid=True) ),
+        alt.Y('Valor Numerico:Q', sort="ascending", axis=alt.Axis(grid=True,format='$,.2f'), title="FIPE", scale=alt.Scale(domain=[min_value, max_value]))
+    ).interactive()
+    st.altair_chart(chart, use_container_width=True)
+
+def data_frame_graph(lista_de_valores):
+    df = pd.DataFrame(data = lista_de_valores, columns=["Data","Preço"])
+    df['Valor Numerico'] = df['Preço'].replace(r'[^\d,]', '', regex=True).str.replace(',', '.').astype(float)
+    min_valor = df['Valor Numerico'].min()
+    max_valor = df['Valor Numerico'].max()
+
+    return df,min_valor,max_valor
+
+def data_frame_min_max(lista_de_valores):
+    df = pd.DataFrame(data = lista_de_valores, columns=["Data","Preço"])
+    df['Valor Numerico'] = df['Preço'].replace(r'[^\d,]', '', regex=True).str.replace(',', '.').astype(float)
+    min = df['Valor Numerico'].idxmin()
+    max = df['Valor Numerico'].idxmax()
+    # print(min)
+    # print(max)
+    cols = ["Data","Fipe"]
+    rows = [
+        "Preço mínimo",
+        "Preço máximo",
+        ]
+    data = [
+        ([df['Data'][min]],[df['Preço'][min]]),
+        ([df['Data'][max]],[df['Preço'][max]])]
+    new_df = pd.DataFrame(data,columns=cols, index=rows)
+    
+    return new_df
+
+def media(lista_de_valores):
+    df = pd.DataFrame(data = lista_de_valores, columns=["Data","Preço"])
+    df['Valor Numerico'] = df['Preço'].replace(r'[^\d,]', '', regex=True).str.replace(',', '.').astype(float)
+    media = round(df['Valor Numerico'].mean(),2)
+    media = decimal.Decimal(media).quantize(decimal.Decimal("0.00"))
+    
+    # if len(str(media)) <=8:
+    #     centavos = str(media)[len(str(media))-2:]
+    #     centenas = str(media)[len(str(media))-6:len(str(media))-3]
+    #     milhares = str(media)[:len(str(media))-6]
+    #     print("--------------------------------------------------")
+    #     print(media)
+    #     print(centavos)
+    #     print(centenas)
+    #     print(milhares)
+    # if len(str(media)) >=9:
+    #     centavos = str(media)[len(str(media))-2:]
+    #     centenas = str(media)[len(str(media))-6:len(str(media))-3]
+    #     milhares = str(media)[len(str(media))-9:len(str(media))-6]
+    #     milhoes = str(media)[:len(str(media))-7]
+    #     print("--------------------------------------------------")
+    #     print(media)
+    #     print(centavos)
+    #     print(centenas)
+    #     print(milhares)
+    #     print(milhoes)
+
+    return moeda(media)
+
+
+def moeda(valor):
+    centavos = str(valor)[len(str(valor))-2:]
+    print("------------------------------")
+    
+    # print(int(int(len(str(int(media))))/3))
+    # qtd = int(int(len(str(int(media))))/3)
+    # print(qtd)
+    novo_media = str(int(valor))
+    print(novo_media)
+    print("------------------------------")
+    ponto = 1
+    letra2 = ""
+    controle = 1
+    # while count != qtd:
+    for letra in novo_media[::-1]:
+        letra2+=str(letra)
+        # print(letra2)
+        # for num in len(novo_media):
+        #     print(num)
+        # print(len(novo_media))
+        if ponto == 3 and controle < len(novo_media):
+            
+            letra2 = letra2 + "."
+            ponto = 0
+        controle+=1
+        ponto += 1
+        # count+=1
+    print(letra2)
+    letra23 =""
+    for letra in letra2[::-1]:
+        letra23+=str(letra)
+    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+    formatado = "R$ "+letra23+","+centavos
+    return formatado
